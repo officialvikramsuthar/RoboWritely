1fe22599de940fd2dc7cbee0064a94d9ce685da5
vikram_suthar9890 <vikram.suthar@kitomba.com>
2023-08-18 09:59:56 +0530

    Added selenium code

--- ContentCreation/crons.py
+++ ContentCreation/crons.py
@@ -3,7 +3,9 @@ from django_cron import CronJobBase, Schedule
 from .models import Content
 import csv
 import openai
-
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 from .utils import generate_content_with_keywords
 
 
@@ -17,8 +19,37 @@ class GenerateContentCronJob(CronJobBase):
         # Set up your OpenAI API key
         file_path =
         with open('.csv', 'r') as csv_file:
-            csv_reader = csv.reader(csv_file)
-            for row in csv_reader:
-                keywords = row[0]
-                content = generate_content_with_keywords(keywords)
-                Content.objects.create(keywords=keywords, content=content)
+                            
+                # Path to your WebDriver executable (e.g., chromedriver.exe)
+                driver_path = '/path/to/your/webdriver'
+
+                # URL of the website you want to visit (e.g., ChatGPT interface)
+                website_url = 'https://www.example.com'
+
+                # Text you want to type into the chat
+                input_text = "Hello, ChatGPT!"
+
+                # Create a new instance of the web driver (e.g., Chrome)
+                driver = webdriver.Chrome(executable_path=driver_path)
+
+                # Open the website
+                driver.get(website_url)
+
+                # Wait for some time to let the page load
+                time.sleep(5)
+
+                # Find the chat input element by its HTML attribute (inspect the webpage to get the appropriate selector)
+                input_element = driver.find_element_by_css_selector('input[type="text"]')
+
+                # Type the input_text into the chat
+                input_element.send_keys(input_text)
+
+                # Press the Enter key to send the message
+                input_element.send_keys(Keys.RETURN)
+
+                # Wait for a few seconds to see the result
+                time.sleep(3)
+
+                # Close the browser
+                driver.quit()
+
